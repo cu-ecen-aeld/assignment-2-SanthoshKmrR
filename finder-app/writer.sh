caf0cd30d3c73a5200f4952fae66f159aabba5a1
SanthoshKmrR <santhosh.kumar@microchip.com>
2026-06-22 10:06:51 +0530

    Assignment-2 commit

---
+++ finder-app/writer.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+ 
+if [ $# -ne 2 ]
+
+then
+
+	echo "Error : Two arguments needed : Filepath and text string"
+
+	exit 1
+
+fi
+ 
+writefile=$1
+
+writestr=$2
+ 
+pathofdir=$(dirname "$writefile")
+
+#assuming already folder is there
+#if [ ! -d "$pathofdir" ]
+#then
+#	mkdir -p "$pathofdir"
+#fi
+ 
+echo "$writestr" > "$writefile"
+ 
+if [ $? -ne 0 ]
+then
+	echo "Error : Unable to create file"
+	exit 1
+fi
+
+exit 0
